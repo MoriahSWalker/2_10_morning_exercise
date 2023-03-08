f7afe55aa90fff4799ad9c1527d0dfb765ae20e5
MoriahSWalker git config --global user.email riahrosebuds@gmail.com <you@example.com>
2023-03-08 09:35:37 -0500

    first commit

---
+++ notes.js
@@ -0,0 +1,19 @@
+// Looking at our SEI Cafe App, consider how we can make it.
+
+// Submit a list of:
+// 1. pages and components we will need to make this app.
+// Looks like a login page, a new order page and a order history page
+// Possibly components for logo, menu items and catergories
+
+// 2. Mongodb collections needed
+// users, order history, menu items
+
+// 3. what our data will look like in those collections
+// data will be objects
+
+// 4. Pieces of STATE in our application (how many, names, and in which components)
+// order quantity, if user is logged in or not,
+
+// 5. routes in our Express server
+// need a route to add items to order
+// route to add new user
